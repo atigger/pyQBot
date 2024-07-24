3c8170b4f5abeab032ffaf1b5b0e838f3f0f5ad8
段薛虎 <649953543@qq.com>
2024-07-24 16:21:25 +0800

    塔罗牌问题修复

--- qBot/plugins/getTarot/get_tarot.py
+++ qBot/plugins/getTarot/get_tarot.py
@@ -1,25 +1,36 @@
 import os
-
-import requests
-from aiocqhttp import MessageSegment, Message
+import httpx
+from aiocqhttp import MessageSegment
 
 from qBot import api, utils
 from qBot.plugins import config
 
+error_msg = "啊偶，抽取失败了，请再试一次吧！"
 
-async def get_tarot() -> Message:
+
+async def get_tarot():
     """
     获取塔罗牌
     :return 塔罗牌结果
     """
-    url = api.TAROT_URL
-    response = requests.get(url)
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(api.TAROT_URL)
+            response.raise_for_status()
+        except httpx.RequestError:
+            return error_msg
+
     data = response.json()
-    tarot_pic_path = os.path.join(config.DATA_DIR, 'cache', 'tarot', data['pic'] + ".png")
+    tarot_pic_path = os.path.join(config.DATA_DIR, 'cache', 'tarot', f"{data['pic']}.png")
+
     if not os.path.exists(tarot_pic_path):
-        await utils.downLoadFile(api.TAROT_IMG_URL + data['pic'] + '.png', tarot_pic_path)
+        if not await utils.downLoadFile(f"{api.TAROT_IMG_URL}{data['pic']}.png", tarot_pic_path):
+            return error_msg
+
     if data['position'] == "逆位":
-        if not os.path.exists(os.path.join(config.DATA_DIR, 'cache', 'tarot', data['pic'] + '-逆位.png')):
-            tarot_pic_path = await utils.turn_image(tarot_pic_path)
+        ni_tarot_pic_path = os.path.join(config.DATA_DIR, 'cache', 'tarot', f"{data['pic']}-逆位.png")
+        tarot_pic_path = await utils.turn_image(tarot_pic_path) if not os.path.exists(
+            ni_tarot_pic_path) else ni_tarot_pic_path
+
     msg = data['title'] + MessageSegment.image(utils.img_to_base64(tarot_pic_path)) + data['description']
     return msg
